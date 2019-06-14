439c8117e6540ca8d7d0e5484b9ac1888865e082
adeola2310 <adekoyejoadeola@gmail.com>
2019-06-14 18:24:40 +0100

    I wrote a function to reverse integer without the use of any data structures like array etc

--- src/sortAlgorithm/ReverseInteger.java
+++ src/sortAlgorithm/ReverseInteger.java
@@ -2,15 +2,19 @@ package sortAlgorithm;
 
 public class ReverseInteger {
     public static int reverse(int num){
+        if (num<0){
+            num = num * -1;
+    }
         String numToString = Integer.toString(num);
+
         StringBuilder builder = new StringBuilder();
         int numLen = numToString.length();
         for (int i = numLen - 1; i > -1; i--){
             builder.append(numToString.charAt(i));
         }
-       return Integer.valueOf(builder.toString());
+       return Integer.valueOf(builder.toString()) * -1;
     }
     public static void main(String[] args) {
-        System.out.println(reverse(345666376));
+        System.out.println(reverse(-345666376));
     }
 }
