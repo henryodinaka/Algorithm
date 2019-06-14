b17da8fe7491faadbe65f16bde95ad4139aca33a
adeola2310 <adekoyejoadeola@gmail.com>
2019-06-14 18:53:14 +0100

    added another method for doing same thng in a more smarter way

--- src/sortAlgorithm/ReverseInteger.java
+++ src/sortAlgorithm/ReverseInteger.java
@@ -4,7 +4,7 @@ public class ReverseInteger {
     public static int reverse(int num){
         boolean isNegative =false;
         if (num<0){
-            num = num * -1;
+            num = -num;
             isNegative = true;
 
     }
@@ -18,7 +18,27 @@ public class ReverseInteger {
         if (isNegative) return Integer.valueOf(builder.toString()) * -1;
          else return Integer.valueOf(builder.toString());
     }
+
+    static int a4(int n)
+    {
+        int sign = 1;
+        if (n == 0) return 0;
+        if (n < 0)
+        {
+            sign = -1;
+            n = -n;
+        }
+        int reverse = 0;
+        while (n != 0)
+        {
+            reverse = (reverse * 10) + (n % 10); /**/
+            n /= 10;
+        }
+        return sign * reverse;
+    }
+
     public static void main(String[] args) {
         System.out.println(reverse(-8984567));
+        System.out.println(a4(-8984567));
     }
 }
