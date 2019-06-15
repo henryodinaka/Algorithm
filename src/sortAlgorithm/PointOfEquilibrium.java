2231dae34db781a08a82e4049bb365b202ec8835
=henryodinaka <=deleogold@gmail.com>
2019-06-15 18:29:31 +0100

    I wrote a function that returns the center of equilibrium of an array of n integers

---
+++ src/sortAlgorithm/PointOfEquilibrium.java
@@ -0,0 +1,60 @@
+package sortAlgorithm;
+
+public class PointOfEquilibrium {
+   public static int findPoint(int[] arr)
+   {
+       int forward = 0;
+       int reverse = 0;
+       int pointA = 0;
+       int pointB = 0;
+       int moveCount = 1;
+
+       for (int i = 0; i<arr.length; i++){
+
+           if (i == 0) {
+               forward += arr[i];
+               reverse += arr[(arr.length - 1) - pointB];
+               moveCount++;
+               continue;
+           }
+
+           if (i !=0 && ((arr.length -moveCount) != 1) && (forward ==reverse)) {
+               forward += arr[i];
+               reverse += arr[(arr.length - 1) - pointB];
+               moveCount+=2;
+               pointA++;
+               pointB++;
+               continue;
+           }
+           if ((arr.length -moveCount) == 1 && (forward ==reverse)){
+               return pointA + 1;
+           }
+           if(i!=0 && forward < reverse){
+               pointA++;
+               forward += arr[pointA];
+               moveCount++;
+               continue;
+           }
+           if (i!=0 && reverse < forward){
+               pointB++;
+               reverse += arr[(arr.length - 1) - pointB];
+               moveCount++;
+
+           }
+
+
+       }
+       return -1;
+   }
+    public static void main(String[] args) {
+
+//       int [] ar= {1, 8, 3, 7, 10, 2};
+       int [] ar= {1,1,5,3,1,1,1,1,1,1,1};
+//        int [] ar= {2, 1, 1, 1, 2, 1, 7};
+//       int [] ar= {1, 2, 3};
+//       int [] ar= {3, 4, 5, 10};
+//       int [] ar= {1, 2, 10, 3, 4};
+
+        System.out.println(findPoint(ar));
+    }
+}
