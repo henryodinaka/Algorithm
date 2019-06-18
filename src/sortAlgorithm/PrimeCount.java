1aea7e7d06e74506c0cb3649a443276d756360d0
=henryodinaka <=deleogold@gmail.com>
2019-06-18 04:31:19 +0100

    I have completed the prime count question

--- src/sortAlgorithm/PrimeCount.java
+++ src/sortAlgorithm/PrimeCount.java
@@ -6,40 +6,44 @@ import java.util.List;
 public class PrimeCount {
 
     public static int primeCount(int start, int end){
-        int num = start;
+
         List<Integer> prime  = new ArrayList<>();
-        boolean isPrime;
-        while (num <=end){
 
-            isPrime = false;
+        boolean isPrime = false;
 
-            if (num == 2 || num ==3)isPrime=true;
+        for (int i = start; i < end+1; i++){
 
-            double sqrt = Math.sqrt(num);
-            System.out.println("Sqrt of "+num+" is "+sqrt);
-            if (sqrt > 1){
-                for (int i =2 ; i < sqrt; i++){
-
-                    if ((sqrt - Math.floor(sqrt)) ==0) break;
-
-                    if (num % 2 ==0) break;
-
-                    if (i > 1 && num % i == 0) break;
-//                    if () break;
-                    else isPrime=true;
-                }
+            if (i == 2 || i ==3) {
+                prime.add(i);
+                continue;
             }
-            if (isPrime) prime.add(num);
-            num++;
+            if (i % 2 ==0) continue; // continue if the number is even
+
+            double sqrt = Math.sqrt(i);
+
+            if (sqrt - Math.floor(sqrt) ==0) continue; //continue if it has a perfect square root
+
+            for (int j =2 ; j < sqrt; j++){
+
+
+                if (i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+
+                else isPrime=true;
+            }
+            if (isPrime) prime.add(i);
+            isPrime = false;
         }
-        prime.forEach(p -> System.out.print(p+" "));
-        System.out.println("Total count ");
+//        prime.forEach(p -> System.out.print(p+" "));
+        System.out.print("\nTotal prime numbers between "+start+" and "+end+" is :");
         return prime.size();
     }
 
     public static void main(String[] args) {
 
-        int i = primeCount(2, 30);
+        int i = primeCount(2, 10000000);
         System.out.println(i);
 
     }
